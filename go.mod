fdd40c9f01d6aef6bc1aab8aefb2229c39f4b72a
Rob Greene <robgreene@gmail.com>
2026-02-28 18:04:50 -0600

    Start of report command.

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module bosh-blob-experiment
 
 go 1.25.0
 
-require gopkg.in/yaml.v3 v3.0.1 // indirect
+require (
+	github.com/rodaine/table v1.3.0 // indirect
+	github.com/urfave/cli/v3 v3.6.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,33 +1,48 @@
 package main
 
 import (
-	"archive/tar"
 	"bosh-blob-experiment/manifest"
-	"compress/gzip"
-	"flag"
-	"fmt"
-	"io"
+	"context"
 	"io/fs"
-	"net/http"
+	"maps"
 	"os"
 	"path/filepath"
 
+	tbl "github.com/rodaine/table"
+	cli "github.com/urfave/cli/v3"
 	"gopkg.in/yaml.v3"
 )
 
-func main() {
-	projectDir := flag.String("project", ".", "project directory")
-	flag.Parse()
+func findProjectReleaseManifests(projectDir string) ([]manifest.ReleaseManifest, error) {
+	var releases []manifest.ReleaseManifest
+	finalBuildsDir := filepath.Join(projectDir, "releases")
+	err := filepath.WalkDir(finalBuildsDir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		if "index.yml" != d.Name() {
+			b, err := os.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			rel := manifest.ReleaseManifest{}
+			err = yaml.Unmarshal(b, &rel)
+			if err != nil {
+				return err
+			}
+			releases = append(releases, rel)
+		}
+		return nil
+	})
+	return releases, err
+}
 
-	if flag.NArg() < 1 {
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	fmt.Printf("dir=%s\n", *projectDir)
-
-	blobMap := map[string]string{}
-	finalBuildsDir := filepath.Join(*projectDir, ".final_builds")
+func findProjectBlobs(projectDir string) (map[string]manifest.Build, error) {
+	blobMap := map[string]manifest.Build{}
+	finalBuildsDir := filepath.Join(projectDir, ".final_builds")
 	err := filepath.WalkDir(finalBuildsDir, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
@@ -45,70 +60,165 @@ func main() {
 			if err != nil {
 				return err
 			}
-			for _, b := range pkg.Builds {
-				blobMap[b.Sha1] = b.BlobstoreId
-			}
+			maps.Copy(blobMap, pkg.Builds)
 		}
 		return nil
 	})
+	return blobMap, err
+}
+
+func generateReport(_ context.Context, cmd *cli.Command) error {
+	projectDir := cmd.String("project")
+	blobs, err := findProjectBlobs(projectDir)
+	if err != nil {
+		return err
+	}
+	releases, err := findProjectReleaseManifests(projectDir)
+	if err != nil {
+		return err
+	}
+	tbl := tbl.New("Version", "Type", "Name", "Blob Name", "Present?")
+	for _, release := range releases {
+		version := release.Version // Note: Only showing on the first entry
+		for _, job := range release.Jobs {
+			blobId := "-"
+			present := "No"
+			blob, ok := blobs[job.Version]
+			if ok {
+				blobId = blob.BlobstoreId
+				present = "Yes"
+			}
+			tbl.AddRow(version, "Job", job.Name, blobId, present)
+			version = ""
+		}
+		for _, pkg := range release.Packages {
+			blobId := "-"
+			present := "No"
+			blob, ok := blobs[pkg.Version]
+			if ok {
+				blobId = blob.BlobstoreId
+				present = "Yes"
+			}
+			tbl.AddRow(version, "Package", pkg.Name, blobId, present)
+			version = ""
+		}
+	}
+	tbl.Print()
+	return nil
+}
+
+func main() {
+	cmd := &cli.Command{
+		Name:        "bbx",
+		Description: "bosh blob experiment",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "project",
+				Aliases:     []string{"p"},
+				Usage:       "project directory",
+				Sources:     cli.EnvVars("BBX_PROJECT"),
+				Local:       false,
+				DefaultText: ".",
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:        "report",
+				Usage:       "Report on blob status",
+				Description: "Generate a report of expected blobs for each final release version",
+				Action:      generateReport,
+			},
+		},
+	}
+
+	err := cmd.Run(context.Background(), os.Args)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(blobMap)
 
-	for i, arg := range flag.Args() {
-		fmt.Printf("%d: %s\n", i, arg)
+	// blobMap := map[string]string{}
+	// finalBuildsDir := filepath.Join(projectDir, ".final_builds")
+	// err := filepath.WalkDir(finalBuildsDir, func(path string, d fs.DirEntry, err error) error {
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	if d.IsDir() {
+	// 		return nil
+	// 	}
+	// 	if "index.yml" == d.Name() {
+	// 		b, err := os.ReadFile(path)
+	// 		if err != nil {
+	// 			return err
+	// 		}
+	// 		pkg := manifest.PackageManifest{}
+	// 		err = yaml.Unmarshal(b, &pkg)
+	// 		if err != nil {
+	// 			return err
+	// 		}
+	// 		for _, b := range pkg.Builds {
+	// 			blobMap[b.Sha1] = b.BlobstoreId
+	// 		}
+	// 	}
+	// 	return nil
+	// })
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(blobMap)
 
-		tmp, err := os.CreateTemp("", "release-")
-		if err != nil {
-			panic(err)
-		}
-		defer func() {
-			tmp.Close()
-			os.Remove(tmp.Name())
-		}()
-		fmt.Printf("tmp=%s\n", tmp.Name())
+	// for i, arg := range flag.Args() {
+	// 	fmt.Printf("%d: %s\n", i, arg)
 
-		resp, err := http.Get(arg)
-		if err != nil {
-			panic(err)
-		}
-		defer resp.Body.Close()
+	// 	tmp, err := os.CreateTemp("", "release-")
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	defer func() {
+	// 		tmp.Close()
+	// 		os.Remove(tmp.Name())
+	// 	}()
+	// 	fmt.Printf("tmp=%s\n", tmp.Name())
 
-		n, err := io.Copy(tmp, resp.Body)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("Bytes copied: %d\n", n)
+	// 	resp, err := http.Get(arg)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	defer resp.Body.Close()
 
-		_, err = tmp.Seek(0, io.SeekStart)
-		if err != nil {
-			panic(err)
-		}
+	// 	n, err := io.Copy(tmp, resp.Body)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	fmt.Printf("Bytes copied: %d\n", n)
 
-		gz, err := gzip.NewReader(tmp)
-		if err != nil {
-			panic(err)
-		}
+	// 	_, err = tmp.Seek(0, io.SeekStart)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
 
-		tarFile := tar.NewReader(gz)
-		for {
-			h, err := tarFile.Next()
-			if err == io.EOF {
-				break
-			} else if err != nil {
-				panic(err)
-			}
+	// 	gz, err := gzip.NewReader(tmp)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
 
-			if "release.MF" == h.Name {
-				meta := manifest.ReleaseManifest{}
-				decoder := yaml.NewDecoder(tarFile)
-				err = decoder.Decode(&meta)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println(meta)
-			}
-		}
-	}
+	// 	tarFile := tar.NewReader(gz)
+	// 	for {
+	// 		h, err := tarFile.Next()
+	// 		if err == io.EOF {
+	// 			break
+	// 		} else if err != nil {
+	// 			panic(err)
+	// 		}
+
+	// 		if "release.MF" == h.Name {
+	// 			meta := manifest.ReleaseManifest{}
+	// 			decoder := yaml.NewDecoder(tarFile)
+	// 			err = decoder.Decode(&meta)
+	// 			if err != nil {
+	// 				panic(err)
+	// 			}
+	// 			fmt.Println(meta)
+	// 		}
+	// 	}
+	// }
 }
